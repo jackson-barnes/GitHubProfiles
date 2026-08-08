29bac252189016fc38dede9ccb784e083db50a16
jackson-barnes <jackson.barnes162@gmail.com>
2026-08-08 10:43:25 +0100

    Initial Commit

---
+++ GitHubProfiles/GitHubProfilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProfilesApp.swift
+//  GitHubProfiles
+//
+//  Created by pina lina on 08/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProfilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
